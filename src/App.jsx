ccb6331a374a0a0c7a2829c4e960ba833672c921
DevArthuro <carlosorrego5151@gmail.com>
2024-03-27 18:07:55 -0500

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import PageNotFound from "./pages/PageNotFound"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
 const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+      errorElement: <PageNotFound/>
+    }
+  ])
 
   return (
     <>
-      Main aplication
+      <Navbar/>
+      <RouterProvider router={router}/>
+      <Footer/>
     </>
   )
 }
