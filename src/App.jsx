156007cc3620d3ae588cc8f375d33d3285b5bdc3
DevArthuro <145373076+DevArthuro@users.noreply.github.com>
2024-03-27 22:52:32 -0500

    Release/add navigation (#2)
    * feat: add router
    
    * fix: change react router structure
    
    * feat(components): add navbar to navigate and logo (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-const App = () => {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+const App = () => {
   return (
     <>
-      Main aplication
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
