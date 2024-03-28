ffad9e736cd12ac5bb13465e3f80689b02e5fc2a
DevArthuro <carlosorrego5151@gmail.com>
2024-03-28 15:01:17 -0500

    feat: create hero banner and fix responsive design

---
+++ src/components/HeroBanner.jsx
@@ -0,0 +1,74 @@
+import { Box, Stack, Typography, Button } from "@mui/material";
+import ImageBanner from "../assets/images/banner-hero-image.png";
+
+const HeroBanner = () => {
+  return (
+    <Stack
+      direction={{ lg: "row", xs: "column" }}
+      position="relative"
+      zIndex={5}
+      sx={{
+        mb: { lg: "100px", xs: "20px" },
+      }}
+    >
+      <Typography
+        sx={{
+          top: { lg: "95%" },
+          left: { lg: "10%", xl: "20%" },
+          translate: "transform(-50%, -50%)",
+          opacity: "0.2",
+        }}
+        display={{ lg: "block", xs: "none" }}
+        fontWeight={600}
+        color="#FF2625"
+        position="absolute"
+        zIndex={1}
+        fontSize="100px"
+      >
+        Práctico y al grano
+      </Typography>
+      <Box
+        sx={{
+          mt: { lg: "90px", xs: "70px" },
+          ml: { sm: "50px" },
+        }}
+        position="relative"
+        p="20px"
+      >
+        <Typography color="#FF2625" fontWeight="600" fontSize="2rem">
+          El club del cuidado físico
+        </Typography>
+        <Typography
+          fontWeight={700}
+          sx={{
+            fontSize: { lg: "44px", xs: "35px" },
+          }}
+        >
+          Tu salud es nuestra prioridad
+        </Typography>
+        <Typography fontSize="22px" lineHeight="35px" mb={2}>
+          Los ejercicios más completos que verás
+        </Typography>
+        <Button
+          variant="contained"
+          href="#exercises"
+          color="error"
+          sx={{ background: "#FF2625" }}
+        >
+          Ver Ejercicios
+        </Button>
+      </Box>
+      <Box display={{ lg: "block", xs: "none" }}>
+        <img
+          src={ImageBanner}
+          alt="banner"
+          className="hero-banner"
+          width={800}
+          height={500}
+        />
+      </Box>
+    </Stack>
+  );
+};
+
+export default HeroBanner;
