77cd6125ab3d48b1886f610c02fd66f72bf4cf81
DevArthuro <carlosorrego5151@gmail.com>
2024-03-27 22:05:46 -0500

    fix: change react router structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "./pages/Home"
-import PageNotFound from "./pages/PageNotFound"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-      errorElement: <PageNotFound/>
-    }
-  ])
-
   return (
     <>
-      <Navbar/>
-      <RouterProvider router={router}/>
-      <Footer/>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
