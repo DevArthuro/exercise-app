a38e4807330e1d31e035d96e28a578853973f1da
DevArthuro <carlosorrego5151@gmail.com>
2024-04-02 15:32:13 -0500

    refactor: changes place to states and improve handle error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,40 @@
 import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Loader from "../components/ui/Loader";
+import { RapidApiExercises } from "../utils/fetchExercisesRapidapi";
 
 const Home = () => {
   const [loader, setLoader] = useState(false);
+  const [exercisesList, setExercisesList] = useState([]);
+  const [filterExercises, setFilterExercises] = useState([]);
+  const [bodyPartsMenu, setBodyPartsMenu] = useState([]);
+
+  useEffect(() => {
+    const fetchBodyParts = async () => {
+      try {
+        const data = await new RapidApiExercises().onlyBodyParts();
+        setBodyPartsMenu(["Todas", ...data]);
+      } catch (error) {
+        setBodyPartsMenu([]);
+        return;
+      }
+    };
+
+    fetchBodyParts();
+  }, []);
   return (
     <>
       {!loader ? (
         <Box>
           <HeroBanner />
-          <SearchExercises setLoader={setLoader} />
+          <SearchExercises
+            setLoader={setLoader}
+            exercisesList={exercisesList}
+            setExercisesList={setExercisesList}
+            setFilterExercises={setFilterExercises}
+          />
         </Box>
       ) : (
         <Box
