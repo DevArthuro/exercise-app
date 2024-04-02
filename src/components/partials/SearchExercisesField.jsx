b615121829471cb5b27317aab3466a4c51e7d60d
DevArthuro <carlosorrego5151@gmail.com>
2024-04-02 10:21:44 -0500

    fix(partials): separate component field

---
+++ src/components/partials/SearchExercisesField.jsx
@@ -0,0 +1,50 @@
+import { Box, Button, TextField } from "@mui/material";
+import PropTypes from "prop-types";
+
+const SearchExercisesField = ({ search, setSearch, handleSearch }) => {
+  return (
+    <Box position="relative" m="65px 0" flexDirection="row">
+      <TextField
+        sx={{
+          height: "76px",
+          input: {
+            fontWeight: 700,
+            border: "none",
+            borderRadius: "4px",
+          },
+          width: { lg: "1170px", sm: "350px", xs: "100%" },
+          backgroundColor: "#fff",
+          borderRadius: "40px",
+        }}
+        value={search}
+        onChange={(event) => {
+          setSearch(event.target.value.toLowerCase());
+        }}
+        placeholder="Busca un ejercicio en particular..."
+        type="text"
+      />
+      <Button
+        className="search-btn"
+        variant="contained"
+        color="error"
+        sx={{
+          height: "55px",
+          background: "#FF2625",
+          m: { sm: "0 5px", xs: "0" },
+          width: { sm: "100px", xs: "100%" },
+        }}
+        onClick={handleSearch}
+      >
+        Buscar
+      </Button>
+    </Box>
+  );
+};
+
+SearchExercisesField.propTypes = {
+  search: PropTypes.string.isRequired,
+  setSearch: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+};
+
+export default SearchExercisesField;
