bececf7023558ba44e0c832b86d11affb4119640
DevArthuro <carlosorrego5151@gmail.com>
2024-04-02 10:21:44 -0500

    fix(partials): separate component field

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { Typography, Stack } from "@mui/material";
 import { RapidApiExercises } from "../utils/fetchExercisesRapidapi";
+import SearchExercisesField from "./partials/SearchExercisesField";
 
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
@@ -41,41 +42,11 @@ const SearchExercises = () => {
       >
         Te van a sorprender estos ejercicios
       </Typography>
-      <Box position="relative" m="65px 0" flexDirection="row">
-        <TextField
-          sx={{
-            height: "76px",
-            input: {
-              fontWeight: 700,
-              border: "none",
-              borderRadius: "4px",
-            },
-            width: { lg: "1170px", sm: "350px", xs: "100%" },
-            backgroundColor: "#fff",
-            borderRadius: "40px",
-          }}
-          value={search}
-          onChange={(event) => {
-            setSearch(event.target.value.toLowerCase());
-          }}
-          placeholder="Busca un ejercicio en particular..."
-          type="text"
-        />
-        <Button
-          className="search-btn"
-          variant="contained"
-          color="error"
-          sx={{
-            height: "55px",
-            background: "#FF2625",
-            m: { sm: "0 5px", xs: "0" },
-            width: { sm: "100px", xs: "100%" },
-          }}
-          onClick={handleSearch}
-        >
-          Buscar
-        </Button>
-      </Box>
+      <SearchExercisesField
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
     </Stack>
   );
 };
