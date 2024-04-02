48935a5746598d6653e215aae2e9b4894d7a88e9
DevArthuro <carlosorrego5151@gmail.com>
2024-04-02 10:55:49 -0500

    feat: add loader search exercises

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
 import { Box } from "@mui/material";
 import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
+import { useState } from "react";
+import Loader from "../components/ui/Loader";
 
 const Home = () => {
+  const [loader, setLoader] = useState(false);
   return (
-    <Box>
-      <HeroBanner />
-      <SearchExercises />
-    </Box>
+    <>
+      {!loader ? (
+        <Box>
+          <HeroBanner />
+          <SearchExercises setLoader={setLoader} />
+        </Box>
+      ) : (
+        <Box
+          width="100%"
+          textAlign="center"
+          height="70vh"
+          alignContent="center"
+        >
+          <Loader />
+        </Box>
+      )}
+    </>
   );
 };
 
