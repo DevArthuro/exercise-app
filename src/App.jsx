34a70197327422bde818196c9ad9f8a86d6605f4
DevArthuro <carlosorrego5151@gmail.com>
2024-04-04 16:49:14 -0500

    feat: add route to show details

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "../src/styles/GlobalStyles.css";
+import ExercisesDetails from "./pages/ExercisesDetails";
 const App = () => {
   return (
     <>
@@ -11,6 +12,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/exercise/{id}" element={ExercisesDetails} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
         <Footer />
